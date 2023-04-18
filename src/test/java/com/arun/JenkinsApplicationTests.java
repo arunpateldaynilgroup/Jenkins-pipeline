a41758a4931a402efad185177c5febb7177cdafb
arun <arun.patel@daynilgroup.com>
2023-04-18 23:52:21 +0530

    jenkun file add

--- src/test/java/com/arun/JenkinsApplicationTests.java
+++ src/test/java/com/arun/JenkinsApplicationTests.java
@@ -1,5 +1,7 @@
 package com.arun;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class JenkinsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(4, 2+2);
 	}
 
 }
